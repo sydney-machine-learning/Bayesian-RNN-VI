e55519dc4dac6c078c81d4a10933f0fa0ec50388
anshul22negi <56079693+anshul22negi@users.noreply.github.com>
2021-11-09 11:25:41 +0530

    Update train_sunspots_vanillarnn.py

--- cyclone/train_sunspots_vanillarnn.py
+++ cyclone/train_sunspots_vanillarnn.py
@@ -1,6 +1,8 @@
 import torch
-from dataloader import trainloader,testloader
-from VanillaRNN import VanillaRNN
+#from dataloader import trainloader,testloader
+#from VanillaRNN import VanillaRNN
+from south_indian_ocean_dataloader import trainloader
+from VanillaRNN_south_indian_ocean import VanillaRNN
 import torch.nn as nn
 import numpy as np   
 import matplotlib.pyplot as plt
@@ -8,23 +10,24 @@ from torchsummary import summary
 import csv
 import math
 
-results_file_path = r'D:\python\BBB_RNN\vanilla_rnn_results.csv'
+results_file_path = r'D:\python\BBB_RNN\data\ACFinance\RNN_results_10step.csv'
 
 with open(results_file_path, 'w') as f:
     writer = csv.writer(f)
-    header = ['rmse_loss', 'train_correct', 'total_train', 'test_correct', 'total_test']
+    header = ['rmse_train_loss', 'rmse_test_loss']
     writer.writerow(header)
-    num_records = 30
+    num_records = 1
     for k in range(num_records):
         input_dim = 1
         hidden_dim = 5
-        output_dim = 1
+        output_dim = 10
 
         model = VanillaRNN(input_dim, hidden_dim, output_dim)
         optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
         Loss = nn.MSELoss()
-        epochs = 20
-        mse_losses=[]
+        epochs = 30
+        train_mse_losses=[]
+        test_mse_losses = []
         training_seq, outputs = [],[]
         for i in range(1,epochs+1):
             single_loss=0
@@ -41,62 +44,27 @@ with open(results_file_path, 'w') as f:
                 if i==epochs: 
                     training_seq=labels.tolist()
                     outputs = Outputs.tolist()
-            mse_losses.append(single_loss) 
+            train_mse_losses.append(single_loss) 
             print(f'epoch: {i:3} loss: {single_loss.item():10.8f}') 
-
-        train_total,train_correct,test_total,test_correct = 0,0,0,0
-
-        for seq,labels in trainloader:
-            Outputs = model(seq)
-            for x,y in zip(Outputs,labels):
-                if x==y:
-                    train_correct+=1
-                train_total+=1
-
+        test_loss=0
+        '''
         for seq,labels in testloader:
-            Outputs = model(seq)
-            for x,y in zip(Outputs,labels):
-                if x==y:
-                    test_correct+=1
-                test_total+=1         
+            outputs = model(seq)
+            loss = Loss(outputs, labels)
+            test_loss = loss
+        '''
+        rmse_train_loss = math.sqrt(train_mse_losses[-1])
+        #rmse_test_loss = math.sqrt(test_loss)
+        print(rmse_train_loss)
+        #record = [rmse_train_loss, rmse_test_loss]
+        #writer.writerow(record)
         
-        rmse_loss = math.sqrt(mse_losses[-1])
         
-        record = [rmse_loss, train_correct, train_total, test_correct, test_total]
-        writer.writerow(record)
-    
+        saveplot = r'D:\python\BBB_RNN\results\track_pred_vanillarnn.png'
 
-
-
-'''
-saveplot = r'D:\python\BBB_RNN\epochs_10_MSEloss.png'
-'''
-"""
-plt.plot(losses, label = 'Training KL Divergence loss')
-plt.legend()
-plt.savefig(saveplot)
-"""
-"""
-plt.plot(mse_losses, label = 'Training MSE loss')
-plt.legend()
-plt.savefig(saveplot);
-"""
-'''
-plt.plot( training_seq[0], label = 'actual')
-plt.plot(outputs[0], label = 'mean')
-#plt.ylim(0, 1)
-plt.legend();
-#plt.savefig(saveplot);
-plt.show();
-'''
-'''
-print(training_seq[0])
-print(outputs[0])
-print(p5[0])
-print(p95[0])
-'''
-
-
-
-#Accuracy plot
-
+        plt.plot( [item[0] for item in training_seq[0]], [item[1] for item in training_seq[0]], label = 'actual')
+        plt.plot([item[0] for item in outputs[0]], [item[1] for item in outputs[0]], label = 'mean')
+        plt.legend();
+        plt.savefig(saveplot);
+        plt.show();    
+        
